df3ec1349cc7a93c3ff9874ce220330f9096af70
suxxsfe <suxxsfe@gmail.com>
2023-11-10 13:51:22 +0800

    feat: index.js and first component

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+  
+  render(){
+    return (
+      <h1>test</h1>
+    );
+  }
+}
+
+export default App;
+
