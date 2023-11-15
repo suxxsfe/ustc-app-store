7502f6290c0f0986434afb4dcac51011a98d109c
suxxsfe <suxxsfe@gmail.com>
2023-11-15 22:02:44 +0800

    feat: public utilities.js

---
+++ client/src/utilities.js
@@ -0,0 +1,19 @@
+
+const convertToJSON = (res) => {
+  if(!res.ok){
+    throw "API request failed with status "+res.status+" and text: "+res.statusText;
+  }
+  
+  return res.clone()
+            .json()
+            .catch((error) => {
+              return res.text().then((text) => {
+                throw "API request's result could not be convert to JSON object:\n"+text;
+              });
+            });
+}
+
+const post = (endpoint, params = {}) => {
+  console.log("send to server (post): "+params);
+}
+
