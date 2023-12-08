749eb1c9310a2232dd601e34670a011fb9e6d35c
suxxsfe <suxxsfe@gmail.com>
2023-12-08 12:50:02 +0800

    fix: 修正 server POST reply

--- server/apiReply.js
+++ server/apiReply.js
@@ -9,14 +9,14 @@ const router = express.Router();
 router.get("/replies", (req, res) => {
     Reply.find({parent: req.query._id}).then((tmp)=>res.send(tmp));
 });
-router.post("/replies", (req, res) => {
-    const newcomm = new Comment({
+router.post("/reply", (req, res) => {
+    const newcomm = new Reply({
         author:{
             name: "ixed",
             _id: 123,
         },
-        parent:req.query.parent,
-        content:req.query.content,
+        parent:req.body.parent,
+        content:req.body.content,
     });
     newcomm.save();
     res.send(newcomm);
