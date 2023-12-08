38627b0e5cb3fea91516d0f697a0c8c8092dc24a
suxxsfe <suxxsfe@gmail.com>
2023-12-08 13:47:02 +0800

    feat: getTagsByTagIds
    POST /api/appinfo 只传入 tag 的 id
    但 App Collection 将同时存储 tag 的 id 和 name

--- server/apiApp.js
+++ server/apiApp.js
@@ -25,45 +25,60 @@ router.get("/appdownload", (req, res) => {
     App.findOne({_id: req.query._id}).then((tmp)=>{res.send(tmp)});
 });
 
+const getTagsByTagIds = async function (tagIds){
+  return await Promise.all(
+    tagIds.map((tagId) => {
+      return (async() => {
+        return await Tags.findOne({_id: tagId})
+      })();
+    }),
+  );
+
+}
+
 router.post("/appinfo", (req, res) => {
     console.log("kkksc03");
     
     let nowDate = new Date().toLocaleDateString();
   
     if(req.body._id === undefined){
-      console.log(req.body);
-      const newApp = new App({
+        console.log(req.body);
+        
+        getTagsByTagIds(req.body.tags)
+        .then((tags) => new App({
+            name: req.body.name,
+            authors: [{name: "qwerty", _id: 123}],
+            img_url: req.body.img_url,
+            downloads: req.body.downloads,
+            links: req.body.links,
+            tags: tags,
+            platforms: req.body.platforms,
+            describe: req.body.description,
+            createdate: nowDate,
+            updatedate: nowDate,
+        }).save())
+        .then((app) => {
+            console.log("success");
+            res.send(app);
+        });
+    }
+    else{
+      
+      getTagsByTagIds(req.body.tags)
+      .then((tags) => App.update({_id:req.query._id},{$set:{
           name: req.body.name,
-          authors: [{name: "qwerty", _id: 123}],
           img_url: req.body.img_url,
           downloads: req.body.downloads,
           links: req.body.links,
           tags: req.body.tags,
           platforms: req.body.platforms,
           describe: req.body.description,
-          createdate: nowDate,
           updatedate: nowDate,
+      }},exec()))
+      .then((app) => {
+          console.log("success");
+          res.send(app);
       });
-      
-      newApp.save().then((app) => {
-          console.log("success");
-          res.send(app);
-  		});
-    }
-    else{
-        App.update({_id:req.query._id},{$set:{
-            name: req.body.name,
-            img_url: req.body.img_url,
-            downloads: req.body.downloads,
-            links: req.body.links,
-            tags: req.body.tags,
-            platforms: req.body.platforms,
-            describe: req.body.description,
-            updatedate: nowDate,
-        }},exec()).then((app) => {
-          console.log("success");
-          res.send(app);
-        });
     }
 });
 router.get("/search", (req, res) => {
