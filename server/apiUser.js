7932a46a1a225c0b77e7a08338b3fb2c874685d4
zzz <mathdaren@qq.com>
2023-12-12 21:13:27 +0800

    some check work

--- server/apiUser.js
+++ server/apiUser.js
@@ -8,34 +8,23 @@ const jwt = require('jsonwebtoken');
 const SECRET = 'somesecret';
 
 router.get("/userinfo", (req, res) => {
-//const op="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1Njk5Nzg0ZGFhMGUzZjRmM2NiMTY3OSIsImlhdCI6MTcwMjAyNzI3NH0.BZKSpegNfSXl8SKLGJf4ZE6HCReKJ-2RrlObIk0HKZ4";
-    const {id} = jwt.verify(req.body.token, SECRET);
-    User.findOne({_id:id}).then((tmp)=>res.send(tmp));
+    User.findOne({_id:req.body._id}).then((tmp)=>{tmp.password="?????";res.send(tmp)});
 });
 router.get("/userprojects", (req, res) => {
-    const {id} = jwt.verify(req.body.token, SECRET);
-    User.findOne({_id:id}).then((tmp)=>res.send(tmp.projects));
+    User.findOne({_id:req.body._id}).then((tmp)=>res.send(tmp.projects));
 });
-// router.post("/userinfo",(req,res)=>{
-//     const newuser = new User({
-//         name:"未命名",
-//         password:req.body.password,
-//         intro: req.query.intro,
-//         links:req.query.links,
-//     });
-//     newuserser.save();
-//     //如果炸了的话用    newuser.save().then((user) => res.send(user));
-//     res.send(newuser.user._id);
-// })
-
-// router.post("/userupdate", (req, res) => {
-//     let e=req.query;
-//     if(e.intro.length()>1)User.update({_id:e._id},{$set:{intro:e.intro}},exec());
-//     if(e.type.length()>1)User.update({_id:e._id},{$set:{type:e.type}},exec());
-//     if(e.regdate.length()>1)User.update({_id:e._id},{$set:{regdate:e.regdate}},exec());
-//     if(e.visdate.length()>1)User.update({_id:e._id},{$set:{visdate:e.visdate}},exec());
-//     if(e.link.webname.length()>1)User.update({_id:e._id},{$set:{link:e.link}},exec());
-
-// });
+router.post("/userupdate", (req, res) => {
+    const {id} = jwt.verify(String(req.body.authorization.split(' ').pop()), SECRET);
+    User.update({_id:id},{$set:{
+        name: req.body.name,
+        password:req.body.password,
+        intro: req.body.intro,
+        type: req.body.type,
+        regdate: req.body.regdate,
+        visdate: req.body.visdate,
+        projects: req.body.projects,
+        links: req.body.links,
+}},exec());
+});
 
 module.exports = router;
