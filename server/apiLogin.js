4a0db8d6b7cd03474a15d2b7c335f88455e86cbd
suxxsfe <suxxsfe@gmail.com>
2023-12-18 13:05:14 +0800

    feat: 注册后生一个初始头像

--- server/apiLogin.js
+++ server/apiLogin.js
@@ -28,7 +28,10 @@ router.post('/login', async (req, res) => {
         user,
         token
     })
-})
+});
+
+const fs = require("fs");
+const path = require("path");
 router.post("/userCreate", (req, res) => {
     
     User.find({name: req.body.name}).then((users) => {
@@ -49,7 +52,11 @@ router.post("/userCreate", (req, res) => {
             type: "普通用户",
         });
         const token = jwt.sign({ id: String(newuser._id)}, SECRET)
-        newuser.save().then((user) => res.send({user,token}));
+        newuser.save().then((user) => {
+          fs.copyFileSync(path.join(__dirname, "upload", "defaultuserlogo.jpeg"),
+                          path.join(__dirname, "upload", "userlogo", String(user._id)));
+          res.send({user,token});
+        });
     });
 });
 
