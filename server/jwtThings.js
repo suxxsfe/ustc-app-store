4d29ccb89b255b8b6fa99e5b9aabf76fdcf2c67c
suxxsfe <suxxsfe@gmail.com>
2023-12-18 16:15:32 +0800

    fix: jwt

--- server/jwtThings.js
+++ server/jwtThings.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 const SECRET = 'somesecret';
+const App = require("./models/App.js");
+
 function getID(token){
   return jwt.verify(token.split(' ')[1], SECRET).id;
 }
 function checkAuthorityApp(token,id){
-  console.log(token);console.log(id);
-  console.log("Checking token");
-  App.findOne({_id:id}).then((app)=>{
+  return App.findOne({_id:id}).then((app)=>{
     return app.authors.map((author)=>{return String(author._id);}).includes(getID(token));
-  }); 
+  });
 }
 
 function checkAuthorityUser(token,id){
