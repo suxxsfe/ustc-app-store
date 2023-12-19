737967499ea1f7a06fe8a19ff8ff99992e6b16c9
suxxsfe <suxxsfe@gmail.com>
2023-12-19 14:41:03 +0800

    feat: Footer 增加内容

--- client/src/components/modules/Footer.js
+++ client/src/components/modules/Footer.js
@@ -13,6 +13,20 @@ class Footer extends Component{
         <span className="footer-text">
           ustc app store
         </span>
+        <div className="footer-info">
+          <div className="footer-content">
+            <a href="https://github.com/suxxsfe/ustc-app-store">code</a>
+          </div>
+          <div className="footer-content">
+            <a href="https://github.com/suxxsfe/ustc-app-store/issues">issue</a>
+          </div>
+          <div className="footer-content">
+            <a href="https://github.com/Math-rad-round">author</a>
+          </div>
+          <div className="footer-content">
+            <a href="https://github.com/suxxsfe">author</a>
+          </div>
+        </div>
       </div>
     );
   }
